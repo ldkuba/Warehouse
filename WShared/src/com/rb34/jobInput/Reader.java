a169016ad447572d91f42efd84280e8f20354e34
jas555 <jas555@student.bham.ac.uk>
2017-03-04 20:53:34 +0000

    Reader: Improved efficiency + tested cancellation boolean + tested all item details + program doesnt have problems if documents have empty lines at the end anymore

--- WShared/src/com/rb34/jobInput/Reader.java
+++ WShared/src/com/rb34/jobInput/Reader.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Set;
 
 public class Reader {
 
@@ -16,18 +15,19 @@ public class Reader {
 		ArrayList<Drop> dropList = new ArrayList<Drop>();
 		
 		try {
-			File fileJob = new File("myDocs/drops.csv");
-			reader = new BufferedReader(new FileReader(fileJob));
+			File fileDrop = new File("myDocs/drops.csv");
+			reader = new BufferedReader(new FileReader(fileDrop));
 
 			String dropLoc;
-			while ((dropLoc = reader.readLine()) != null) {
+			while ((dropLoc = reader.readLine()) != null && dropLoc.length() > 0) {
+
 				String[] dropInfo = dropLoc.split(",");
 				int xLoc = Integer.parseInt(dropInfo[0]);
 				int yLoc = Integer.parseInt(dropInfo[1]);
 				Drop newDropLoc = new Drop(xLoc,yLoc);
 				dropList.add(newDropLoc);
-
 			}
+			
 			reader.close();
 			return dropList;
 
@@ -55,7 +55,7 @@ public class Reader {
 			reader = new BufferedReader(new FileReader(fileItems));
 
 			String item;
-			while ((item = reader.readLine()) != null) {
+			while ((item = reader.readLine()) != null && item.length() > 0) {
 				String[] itemInfo = item.split(",");
 				String itemID = itemInfo[0];
 				float reward = Float.valueOf(itemInfo[1]);
@@ -71,19 +71,16 @@ public class Reader {
 			reader = new BufferedReader(new FileReader(fileLoc));
 
 			String location;
-			while ((location = reader.readLine()) != null) {
+			int i = 0;
+			while ((location = reader.readLine()) != null && location.length() > 0) {
 				String[] locInfo = location.split(",");
-				String itemID = locInfo[2];
 				int xLoc = Integer.parseInt(locInfo[0]);
 				int yLoc = Integer.parseInt(locInfo[1]);
 				
-				for (int index = 0; index <= itemList.size() - 1; index++) {
-					if(itemList.get(index).getItemID().equals(itemID)){
-						itemList.get(index).setX(xLoc);
-						itemList.get(index).setY(yLoc);
-						break;
-					}
-				}
+				itemList.get(i).setX(xLoc);
+				itemList.get(i).setY(yLoc);
+				i++;
+				
 			}
 			reader.close();
  
@@ -97,27 +94,51 @@ public class Reader {
 			int itemIndex = 9999;
 			String job; 
 			
-			while((job = reader.readLine()) != null){
+			while((job = reader.readLine()) != null && job.length() > 0){
 				String [] jobInfo = job.split(","); 
 				String jobID = jobInfo[0];
 				Job newJob = new Job(jobID);
 				
 				//Checks what items the specific job requires and creates an order object
 				//Order contains [Item, Item Count]
-				for(int i = 1; i <= jobInfo.length -1; i += 2){ 
-					 itemID =  jobInfo[i];
-					 count = Integer.parseInt(jobInfo[i + 1]);
+				for(int index = 1; index <= jobInfo.length -1; index += 2){ 
+					 itemID =  jobInfo[index];
+					 count = Integer.parseInt(jobInfo[index + 1]);
+
 					 
 					 for(int j = 0; j <= itemList.size() - 1; j++){
+						 
 						 if(itemList.get(j).getItemID().equals(itemID)){
-							 itemIndex = i;
+							 itemIndex = j;
+							 Order newOrder = new Order(itemList.get(itemIndex),count);
+							 newJob.addItem(itemID, newOrder);
 							 break;
 						 }
 					 }
-					 Order newOrder = new Order(itemList.get(itemIndex),count);
-					 newJob.addItem(itemID, newOrder);
 				}
 				jobList.add(newJob);
+			}
+			 
+			reader.close();
+			
+			File fileCancellations = new File("myDocs/cancellations.csv");
+			reader = new BufferedReader(new FileReader(fileCancellations));
+			
+			String line;
+			boolean wasCancelled = false;
+			i = 0;
+			while((line = reader.readLine()) != null && line.length() > 0){
+				String [] lineInfo = line.split(","); 
+				String wasCancelledStr = lineInfo[1];
+				if(wasCancelledStr.equals("1")){
+					wasCancelled = true;
+				}
+				else{
+					wasCancelled = false;
+				}
+				jobList.get(i).setCancelled(wasCancelled);
+				i++; 
+				
 			}
 			 
 			reader.close();
@@ -135,20 +156,37 @@ public class Reader {
 	}
 
 	public static void main(String[] args) {
-
-		//Testing jobList
+		
+		//Testing jobList 
 		ArrayList<Job> jobList = createJobList();
-		for (int i = 0; i <= jobList.size()-1 ; i++) {
-			System.out.println(jobList.get(i).getJobId());
-			System.out.println(jobList.get(i).getItemsID());
-			System.out.print("[");
+		for (int i = 0; i <= jobList.size() - 1 ; i++) {
+			System.out.println("Job ID: " + jobList.get(i).getJobId());                   
+			System.out.println("Cancellation: " + jobList.get(i).getCancelled()); 
+			System.out.println("List of Items: "+jobList.get(i).getItemsID());
+			
+			System.out.print("Count for each job: [");
 			Iterator<String>itr = jobList.get(i).getItemsID().iterator();
-		     while(itr.hasNext()) {
+		    while(itr.hasNext()) {
 		         Object element = itr.next();
-		         System.out.print(jobList.get(i).getCount((String)element) + ",");
+		         System.out.print( jobList.get(i).getCount((String) element) + ",");
 		      }
-			System.out.print("]\n");
+		    System.out.print("]\n");
+		    
+		    System.out.print("Item Info: [");
+		    itr = jobList.get(i).getItemsID().iterator();
+		    while(itr.hasNext()) {
+		         Object element = itr.next();
+		         System.out.print(jobList.get(i).getOrderList().get(element).getItem().getItemID() + "/");
+		         System.out.print(jobList.get(i).getOrderList().get(element).getItem().getReward() + "/");
+		         System.out.print(jobList.get(i).getOrderList().get(element).getItem().getWeight() + "/");
+		         System.out.print(jobList.get(i).getOrderList().get(element).getItem().getX() + ".");
+		         System.out.print(jobList.get(i).getOrderList().get(element).getItem().getY() + " <=> ");
+		      }
+		    
+		    System.out.print("]\n");
+			System.out.println("---------------");
 		}
+		
 		
 		//Testing Drop locations
 		System.out.println("----------Drop Location Test ----------");
