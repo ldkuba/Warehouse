2dc9b04811236b07644957ab416fee847bb4e55f
Alexandre SCHWEIG--PETERS <alexandre.peters.schweig@gmail.com>
2017-03-05 16:52:20 +0000

    "Merge" with job_input. Didn't work as we were testing merge previously and not fixed correctly

---
+++ WShared/src/com/rb34/jobInput/interfaces/IJob.java
@@ -0,0 +1,31 @@
+package com.rb34.jobInput.interfaces;
+import java.util.Collection;
+import java.util.HashMap;
+
+//Interface for Job object
+public interface IJob{
+
+	//Gets unique job ID
+	String getJobId();
+	
+	//gets numbers of Items required
+	int getCount(String itemID);
+	
+	//Adds item to Hash Table (Key = item ID , Value = order object)
+	//Order object contains [Item, count];
+	void addItem(String itemID, IOrder order);
+	
+	//returns true or false depending if this job was cancelled previously
+	boolean getCancelled();
+	
+	//sets true or false depending if this job was cancelled previously
+	void setCancelled(boolean wasCancelled);
+
+	//Gets hashMap, where the key is the Item ID and the value is the Order
+	HashMap <String, IOrder> getOrderList ();
+	
+	//Gets all IDs of the items required by the job.
+	Collection <String> getItemsID();
+	
+	
+}
