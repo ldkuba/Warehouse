d02c0caae786138e496bd5c273e86826fa550d5b
jas555 <jas555@student.bham.ac.uk>
2017-03-08 15:01:03 +0000

    Taking into consideration count when calculating net reward + making more in depth test

--- WPC/testSrc/com/rb34/jobSelection/test/rewardSortTest.java
+++ WPC/testSrc/com/rb34/jobSelection/test/rewardSortTest.java
@@ -1,6 +1,9 @@
 package com.rb34.jobSelection.test;
+import com.rb34.jobInput.Item;
 import com.rb34.jobInput.Job;
+import com.rb34.jobInput.Order;
 import com.rb34.jobInput.Reader;
+import com.rb34.jobInput.interfaces.IOrder;
 import com.rb34.jobSelection.*;
 
 import java.util.ArrayList;
@@ -18,7 +21,20 @@ public class rewardSortTest {
 		for (int i = 0; i <= jobList.size() - 1; i++) {
 			System.out.print("Job ID: " + jobList.get(i).getJobId());
 			System.out.println(" -- netReward: " + jobList.get(i).getTotalReward());
-			System.out.println("---------------");
+			
+			Iterator<IOrder> itr = jobList.get(i).getOrderList().values().iterator();
+			while (itr.hasNext()) {
+				IOrder element = itr.next();
+				System.out.println(element.getItem().getReward() + " - " + element.getCount());
+				System.out.println("---------------");
+			}
+			
+			
+			
+			
+			
+			System.out.println("=========================================================");
+			
 		}
 	}
 }
