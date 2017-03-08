f76f003baf2f759abb1f7521aebe2e66fe88e370
Anushan Lingam <axa1186@student.bham.ac.uk>
2017-03-08 22:40:36 +0000

    Forgot to push JUnit Tests

---
+++ WShared/testsrc/com/rb34/route_execution/Execute.java
@@ -0,0 +1,46 @@
+package com.rb34.route_execution;
+
+import com.rb34.route_planning.graph_entities.IVertex;
+import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Execute {
+
+	private ArrayList<String> robotInstructions;
+	private ArrayList<IVertex> vertexPath;
+	
+	private static final Logger log4j = LogManager.getLogger(Execute.class.getName());
+
+	public ArrayList<String> runRoute(ArrayList<IVertex> path) {
+		 vertexPath = path;
+
+			if(vertexPath == null) {
+				log4j.error("Cannot convert to movement: Path is empty or in incorrect format for conversion");
+				throw new IllegalArgumentException("Path is empty");
+			} 
+		convertToMovement converter = new convertToMovement();
+
+		/*boolean debug = false;
+		if (debug  == true) {
+			System.out.println("");
+
+			System.out.println("Chosen Path: ");
+			for (IVertex node : path) {
+				System.out.print(node.getLabel().getName() + "; ");
+			}
+
+			System.out.println("");
+
+		}*/
+		
+		robotInstructions = converter.execute(vertexPath);
+		System.out.println(robotInstructions);
+		
+		//Replace with method that sends to bluetooth
+		log4j.info("Converted Instructions" + robotInstructions);
+		return robotInstructions;
+	}
+
+}
