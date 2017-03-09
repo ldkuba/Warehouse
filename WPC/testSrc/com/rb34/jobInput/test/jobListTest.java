c0f6470718c2f602143b6fd8dc722f273cfdd448
jas555 <jas555@student.bham.ac.uk>
2017-03-09 10:42:57 +0000

    added new JUnit test for sample list creator

--- WPC/testSrc/com/rb34/jobInput/test/jobListTest.java
+++ WPC/testSrc/com/rb34/jobInput/test/jobListTest.java
@@ -14,38 +14,38 @@ import com.rb34.jobInput.Job;
 public class jobListTest {
 
 	
-	//@Test
+	@Test
 	public void JobIDChecks() {
 		ArrayList<Job> jobList = Reader.createJobList();
 		// assert statements
-		assertEquals("JobID should be 10001", "10001", jobList.get(1)
+		assertEquals("10001", jobList.get(1)
 				.getJobId());
-		assertEquals("jobID one should be 10050", "10050", jobList.get(50)
+		assertEquals("10050", jobList.get(50)
 				.getJobId());
-		assertEquals("jobID one should be 12000", "12000", jobList.get(2000)
+		assertEquals("12000", jobList.get(2000)
 				.getJobId());
-		assertEquals("jobID one should be 20000", "20000", jobList.get(10000)
+		assertEquals("20000", jobList.get(10000)
 				.getJobId());
-		assertEquals("jobID one should be 29999", "29999", jobList.get(19999)
+		assertEquals("29999", jobList.get(19999)
 				.getJobId());
 	}
 
-	//@Test
+	@Test
 	public void numOrder() {
 		ArrayList<Job> jobList = Reader.createJobList();
 		// assert statements
-		assertEquals("Number of Orders should be 2 ", 2, jobList.get(0)
+		assertEquals(2, jobList.get(0)
 				.getOrderList().size());
-		assertEquals("jobID one should be 4", 4, jobList.get(25).getOrderList()
+		assertEquals(4, jobList.get(25).getOrderList()
 				.size());
-		assertEquals("jobID one should be 4", 4, jobList.get(75).getOrderList()
+		assertEquals(4, jobList.get(75).getOrderList()
 				.size());
-		assertEquals("jobID one should be 5", 5, jobList.get(1000)
+		assertEquals(5, jobList.get(1000)
 				.getOrderList().size());
 
 	}
 
-	//@Test
+	@Test
 	public void sizeOfJobList() {
 		ArrayList<Job> jobList = Reader.createJobList();
 		// assert statements
@@ -53,9 +53,15 @@ public class jobListTest {
 				jobList.size());
 	}
 
-	//Not set as a test on purpose
 	@Test
-	public void printJobList() {
+	public void sizeOfSampleJobList() {
+		ArrayList<Job> jobList = Reader.createSampleJobList();
+		// assert statements
+		assertEquals(4,jobList.size());
+	}
+	
+	@Test
+	public void printSampleJobList() {
 		ArrayList<Job> jobList = Reader.createSampleJobList();
 		for (int i = 0; i <= jobList.size() - 1; i++) {
 			System.out.println("Job ID: " + jobList.get(i).getJobId());
