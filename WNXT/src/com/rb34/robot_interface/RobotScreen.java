66cc30665e3e9fa2e5f1ce5357175909518a3831
vxm666 <vxm666@cs.bham.ac.uk>
2017-03-09 11:30:18 +0000

    Simple robot interface

--- WNXT/src/com/rb34/robot_interface/RobotScreen.java
+++ WNXT/src/com/rb34/robot_interface/RobotScreen.java
@@ -4,70 +4,33 @@ import lejos.nxt.LCD;
 
 public class RobotScreen {
 	
-	private String xyAxis;
-	private String anyMessage;
-	private String moving = "I am moving";
-	private String waiting = "I am waiting";
-	private LCD lcd = new LCD ();
+	private String state;
+	private int x, y;
 	
+	// Location, state, message
 	// 99, 63
 	
-	public RobotScreen (int x, int y) {
+	public RobotScreen (int x, int y, String state) {
 		xyAxis = "I am at " + x + ":" + y;
+		this.x = x;
+		this.y = y;
+		this.state = state;
 	}
 	
-	private void drawBordersTop () {	
-		for (int x = 0; x <= 99; x++) {
-			for (int y = 0; y <= 5; y++) {
-				LCD.setPixel(x, y, 1);
-			}
-			
-			for (int y = 58; y <= 63; y++) {
-				LCD.setPixel(x, y, 1);
-			}
-		}
-		
-		for (int y = 0; y <= 63; y++) {
-			for (int x = 0; x <= 5; x++) {
-				LCD.setPixel(x, y, 1);
-			}
-			
-			for (int x = 94; x <= 99; x++) {
-				LCD.setPixel(x, y, 1);
-			}
-		}
-	}
-	
-	public void printMessage (String string) {
+	public void printEverything () {
 		LCD.clear();
-		drawBordersTop();
-		LCD.drawString(string, LCD.CELL_WIDTH / 2,LCD.CELL_HEIGHT / 2);
+		LCD.drawString("X:Y ->" + x + ":" + y, LCD.CELL_WIDTH / 4, 4 * LCD.CELL_HEIGHT / 6);
+		LCD.drawString("State: " + state, LCD.CELL_WIDTH / 4, 3 * LCD.CELL_HEIGHT / 6);
 	}
 	
-	public void myLocation () {
-		LCD.clear();
-		drawBordersTop();
-		LCD.drawString(xyAxis,LCD.CELL_WIDTH / 2,LCD.CELL_HEIGHT / 2);;
-	}
-	
-	public void myLocation (int x, int y) {
-		LCD.clear();
-		drawBordersTop();
-		xyAxis = "I am at " + x + ":" + y;
-		LCD.drawString(xyAxis,LCD.CELL_WIDTH / 2,LCD.CELL_HEIGHT / 2);
+	public void printLocation (int x, int y) {
+		this.x = x;
+		this.y = y;
+		printEverything();
 	}
 	
 	public void printState (String state) {
-		LCD.clear();
-		drawBordersTop();
-		
-		switch (state) {
-		case "moving": LCD.drawString("Moving",LCD.CELL_WIDTH / 2,LCD.CELL_HEIGHT / 2);
-		break;
-		case "turning": LCD.drawString("Turning",LCD.CELL_WIDTH / 2,LCD.CELL_HEIGHT / 2);
-		break;
-		default: LCD.drawString("Waiting",LCD.CELL_WIDTH / 2,LCD.CELL_HEIGHT / 2);;
-		break;
-		}
+		this.state = state;
+		printEverything();
 	}
 }
