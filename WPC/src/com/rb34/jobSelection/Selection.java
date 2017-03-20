0b0ebd7819fa035888f48efa1de95072f86be41d
mxc413 <mxc413@cs.bham.ac.uk>
2017-03-20 17:52:36 +0000

    Initial commit

--- WPC/src/com/rb34/jobSelection/Selection.java
+++ WPC/src/com/rb34/jobSelection/Selection.java
@@ -1,19 +1,72 @@
 package com.rb34.jobSelection;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 
-import org.apache.logging.log4j.core.Logger;
+import org.apache.log4j.*;
 
 import com.rb34.jobInput.Job;
+import com.rb34.jobInput.Reader;
+import com.rb34.jobInput.interfaces.IItem;
+import com.rb34.jobInput.interfaces.IOrder;
 
 public class Selection {
 	
+	// For testing.
 	final static Logger logger = Logger.getLogger(Selection.class);
 	
-	public static ArrayList <Job> rewardSelection(ArrayList <Job> jobList){
-		logger.debug("closes cancellation.csv");
-		Collections.sort(jobList,Collections.reverseOrder());
-		logger.debug("Sorts jobList according to netReward of each job");
-		return jobList;
+	public static PriorityQueue<Job> sortJobs() {
+		// Create an empty queue to contain the sorted jobs.
+		PriorityQueue<Job> queue = new PriorityQueue<Job>(10, new JobValueComparator());
+		// Create a new variable, jobValue.
+		// This is used to determine how valuable the total job is,
+		// taking into consideration each item's reward, weight and count.
+		float jobValue = 0.0f;
+		// Get the list of jobs from Reader. Thanks Juliano!
+		ArrayList<Job> jobs = Reader.createJobList();
+		// Iterate through the jobs.
+		Iterator<Job> it = jobs.iterator();
+		int count = 0;
+		while (it.hasNext() && count < 5) {
+			// Extract the next job.
+			// Jobs contain a list of orders.
+			Job job = (Job) it.next();
+			// Extract the list of orders from this job.
+			// The list of orders, surprise surprise, contains orders.
+			HashMap<String, IOrder> orderList = job.getOrderList();
+			// Extract the orderIds so we can access items from the orderList.
+			Collection<String> orderIds = job.getItemsID();
+			for (String o : orderIds) {
+				// Extract the order.
+				// Orders contain items and their count.
+				IOrder order = orderList.get(o);
+				// Extract each item from the order.
+				// Items contain their reward, weight and coordinates.
+				IItem item = order.getItem();
+				// Extract the reward, weight and count of the item.
+				float reward = item.getReward();
+				float weight = item.getWeight();
+				int itemCount = order.getCount();
+				// Calculate an arbitrary item value.
+				// This formula takes the reward and applies a weight, based on the item weight.
+				// The lighter the item, the greater the weight.
+				// We then multiply by the item count.
+				// TO IMPROVE:
+				// Consider the robot travel time in the formula.
+				float orderValue = reward * (1 / weight) * itemCount;
+				// Add this to the total job value.
+				jobValue += orderValue;
+			}
+			// Set the job value, add it to the queue and move onto the next job.
+			job.setJobValue(jobValue);
+			queue.add(job);
+			count++;
+			logger.debug("Job number " + count + " has been added to the queue.");
+		}
+		// Return the sorted list of jobs.
+		logger.debug("Queue has been returned.");
+		return queue;
 	}
 }
