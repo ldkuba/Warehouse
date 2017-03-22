40076ae190cc9a139745466aa3c99ce2bdfab071
bxs633 <bxs633@student.bham.ac.uk>
2017-03-22 15:24:33 +0000

    Nothing changed-still asking me to comming

--- WPC/src/com/rb34/route_execution/Heading.java
+++ WPC/src/com/rb34/route_execution/Heading.java
@@ -1,15 +1,12 @@
 package com.rb34.route_execution;
-
 // Maintains the Heading of the robot
 //Replace with pose class in lejos?
-
 public class Heading {
 	
 	private static String heading = "E";
 	public static String getHeading() {
 		return heading;
 	}
-
 	public static void setHeading(String val) {
 		heading = val;
 	}
