af7559b05d2c0a92d266be2cd3cf05dc81871cb8
Alexandre SCHWEIG--PETERS <axs1386@cs.bham.ac.uk>
2017-03-23 19:01:49 +0000

    Revert "fixed package namesgit add testsrc/git add testsrc/git add testsrc/"
    This reverts commit 878206f3d212041cb9f58ee01bbae79cc820bb26

---
+++ WPC/testsrc/com/rb34/jobInput/test/DropListTest.java
@@ -0,0 +1,30 @@
+package com.rb34.jobInput.test;
+
+import static org.junit.Assert.assertEquals;
+
+import com.rb34.jobInput.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class DropListTest {
+	
+	public Reader reader = new Reader();
+	@Test
+	public void DropTest(){
+		System.out.println("----------Drop Location Test ----------");
+		ArrayList<Drop> dropList = reader.createDropList();
+		for(int i = 0; i <= dropList.size() - 1; i++){
+			System.out.print("X:" + dropList.get(i).getX());
+			System.out.println(" Y:" + dropList.get(i).getY());
+			System.out.println("-----------------");
+		}
+	}
+	@Test
+	public void sizeOfDropList() {
+		ArrayList<Drop> dropList = reader.createDropList();
+		assertEquals("Number of jobs in list should be 2", 2, dropList.size());
+
+	}
+}
